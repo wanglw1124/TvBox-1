8b448c1490d7504870c4bf882eae8b933c8e83b2
tv-player <kindom_90h@126.com>
2022-07-31 23:04:24 +0800

    remove

--- readJson.js
+++
@@ -1,7 +0,0 @@
-const stripJsonComments = require("strip-json-comments");
-
-const fs = require("fs-extra");
-
-let data = fs.readFileSync("./setting/VIP_1/0711.json");
-
-console.log(JSON.parse(stripJsonComments(data.toString())));
